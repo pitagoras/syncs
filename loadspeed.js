9e93deca1925176e6a2c47d71d66d8c3f9888c13
pitagoras <root@joypod.net>
2013-09-03 00:15:48 +0900

    comiit

--- loadspeed.js
+++ loadspeed.js
@@ -2,19 +2,66 @@ var page = require('webpage').create(),
     system = require('system'),
     t, address;
 
-if (system.args.length === 1) {
-    console.log('Usage: loadspeed.js');
-    phantom.exit(1);
-} 
+    t = Date.now();
+	
+    address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everyBatch_TARGET.htm"; //これはcookなので、変えること。
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('FAIL to load the address');
+        } else {
+            t = Date.now() - t;
+            console.log('Page title is ' + page.evaluate(function () {
+                return document.title;
+            }));
+            console.log('Loading time ' + t + ' msec');
+        }
+        phantom.exit();
+    });
 
-t = Date.now();
-address = system.args[1];
-page.open(address, function (status) {
-    if (status !== 'success') {
-        console.log('FAIL to load the address');
-    } else {
-        t = Date.now() - t;
-        console.log('Loading time ' + t + ' msec');
-    }
-    phantom.exit();
-});
+
+
+
+	
+	h = (new Date()).getHours();
+
+	if((h >=  0) && (h < 1)){
+	
+    address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-Daily_TARGET.htm"; //これはcookなので、変えること。
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('FAIL to load the address');
+        } else {
+            t = Date.now() - t;
+            console.log('Page title is ' + page.evaluate(function () {
+                return document.title;
+            }));
+            console.log('Loading time ' + t + ' msec');
+        }
+        phantom.exit();
+    });
+
+	}
+
+
+	
+
+	
+	m = (new Date()).getMinutes();
+
+	if((m >=  15) && (h <= 25)){
+	
+    address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-Weather_TARGET.htm"; //これはcookなので、変えること。
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('FAIL to load the address');
+        } else {
+            t = Date.now() - t;
+            console.log('Page title is ' + page.evaluate(function () {
+                return document.title;
+            }));
+            console.log('Loading time ' + t + ' msec');
+        }
+        phantom.exit();
+    });
+
+	}
