57d7eac23d4cdfacb06f7ff9bdeab52165fbac67
pitagoras <root@joypod.net>
2013-09-02 23:04:58 +0900

    speed

--- loadspeed.js
+++ loadspeed.js
@@ -5,19 +5,16 @@ var page = require('webpage').create(),
 if (system.args.length === 1) {
     console.log('Usage: loadspeed.js');
     phantom.exit(1);
-} else {
-    t = Date.now();
-    address = system.args[1];
-    page.open(address, function (status) {
-        if (status !== 'success') {
-            console.log('FAIL to load the address');
-        } else {
-            t = Date.now() - t;
-            console.log('Page title is ' + page.evaluate(function () {
-                return document.title;
-            }));
-            console.log('Loading time ' + t + ' msec');
-        }
-        phantom.exit();
-    });
-}
+} 
+
+t = Date.now();
+address = system.args[1];
+page.open(address, function (status) {
+    if (status !== 'success') {
+        console.log('FAIL to load the address');
+    } else {
+        t = Date.now() - t;
+        console.log('Loading time ' + t + ' msec');
+    }
+    phantom.exit();
+});
