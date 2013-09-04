66426e2b88fba201a86e35fcce48713df8764113
pitagoras <root@joypod.net>
2013-09-04 19:37:18 +0900

    miy

--- loadspeed.js
+++ loadspeed.js
@@ -3,7 +3,26 @@ var page = require('webpage').create(),
     t, address;
 
     t = Date.now();
-		
+
+	
+	
+    address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everyBatch_TARGET.htm"; //これはcookなので、変えること。
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('FAIL to load the address');
+        } else {
+            t = Date.now() - t;
+            console.log('Page title is ' + page.evaluate(function () {
+                return document.title;
+            }));
+            console.log('Loading time ' + t + ' msec');
+        }
+//        phantom.exit();
+    });
+
+
+
+	
 	h = (new Date()).getHours();
 
 	if((h >=  10) && (h < 11)){
@@ -52,19 +71,7 @@ var page = require('webpage').create(),
 // _sleep(123456);
 
 
-    address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everyBatch_TARGET.htm"; //これはcookなので、変えること。
-    page.open(address, function (status) {
-        if (status !== 'success') {
-            console.log('FAIL to load the address');
-        } else {
-            t = Date.now() - t;
-            console.log('Page title is ' + page.evaluate(function () {
-                return document.title;
-            }));
-            console.log('Loading time ' + t + ' msec');
-        }
-        phantom.exit();
-    });
+
 
 	
 	
