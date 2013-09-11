ca9c7b6155622feddec5833dfe96cdd0e0f8d0b1
pitagoras <root@joypod.net>
2013-09-12 00:01:56 +0900

    commits

--- loadspeed.js
+++ loadspeed.js
@@ -5,7 +5,6 @@ var page = require('webpage').create(),
     t = Date.now();
 
 	
-	
     address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everyBatch_TARGET003.htm"; //これはcookなので、変えること。
     page.open(address, function (status) {
         if (status !== 'success') {
@@ -17,15 +16,14 @@ var page = require('webpage').create(),
             }));
             console.log('Loading time ' + t + ' msec');
         }
-//        phantom.exit();
     });
 
 
-
+	m = (new Date()).getMinutes();
 	
 	h = (new Date()).getHours();
 
-	if (h = 14) { // 23:00 - 24:00	
+	if ((h = 14) && (m <= 12)) { // 22:00 - 23:00
     address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-Daily_TARGET.htm"; //これはcookなので、変えること。
     page.open(address, function (status) {
         if (status !== 'success') {
@@ -37,30 +35,12 @@ var page = require('webpage').create(),
             }));
             console.log('TIME: H:' + h );
         }	
-//        phantom.exit();
     });
 	
 	}
 	
-	if (h = 16) { // 25:00 - 26:00
-// Scoreを「かきまぜる」役目。	
-    address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everyBatch_Score_TARGET.htm"; //これはcookなので、変えること。
-    page.open(address, function (status) {
-        if (status !== 'success') {
-            console.log('FAIL to load the address');
-        } else {
-            t = Date.now() - t;
-            console.log('Page title is ' + page.evaluate(function () {
-                return document.title;
-            }));
-            console.log('Loading time ' + m);
-        }
-//        phantom.exit();
-    });
 	
-	}
-	
-	if (h = 15) { // 24:00 - 25:00
+	if ((h = 15) && (m <= 12)) { // 23:00 - 24:00
 // 日の出・日没時間の洗い替え。
     address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everySun_TARGET003.htm"; //これはcookなので、変えること。
     page.open(address, function (status) {
@@ -73,18 +53,32 @@ var page = require('webpage').create(),
             }));
             console.log('Loading time ' + m);
         }
-//        phantom.exit();
+    });
+	
+	}
+
+	
+	if ((h = 16) && (m <= 12)) { // 24:00 - 25:00
+// Scoreを「かきまぜる」役目。	
+    address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everyBatch_Score_TARGET.htm"; //これはcookなので、変えること。
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('FAIL to load the address');
+        } else {
+            t = Date.now() - t;
+            console.log('Page title is ' + page.evaluate(function () {
+                return document.title;
+            }));
+            console.log('Loading time ' + m);
+        }
     });
 	
 	}
 
 
-// _sleep(123456);	
-
 	
-	m = (new Date()).getMinutes();
 
-	if((m >= 5) && (m <= 20)){
+	if (m >= 48) {
 	
     address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-Weather_TARGET.htm"; //これはcookなので、変えること。
     page.open(address, function (status) {
@@ -97,24 +91,6 @@ var page = require('webpage').create(),
             }));
             console.log('TIME: M:' + m);
         }
-//        phantom.exit();
     });
-	
+    phantom.exit();	
 	}
-	
-	
-// _sleep(123456);
-
-
-
-
-	
-	
-// function _sleep(time){
-//	var d1 = new Date().getTime();
-//	var d2 = new Date().getTime();
-//	while( d2 < d1 + time ){
-//		d2=new Date().getTime();
-//	}
-//	return;
-// }
