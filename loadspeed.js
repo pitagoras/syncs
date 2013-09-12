210b4fc56f482f9bb2719bd77b3d3369ac96d366
pitagoras <root@joypod.net>
2013-09-13 00:10:35 +0900

    great!

--- loadspeed.js
+++ loadspeed.js
@@ -21,7 +21,7 @@ var page = require('webpage').create(),
 	m = (new Date()).getMinutes();	
 	h = (new Date()).getHours();
 
-	if ((h = 14) && (m <= 12)) { // 22:00 - 23:00
+	if ((h = 14) && (m <= 11)) { // 22:00 - 23:00
     address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-Daily_TARGET.htm"; //これはcookなので、変えること。
     page.open(address, function (status) {
         if (status !== 'success') {
@@ -32,15 +32,15 @@ var page = require('webpage').create(),
                 return document.title;
             }));
             console.log('TIME: H:' + h );
-        }	
+        }
+	phantom.exit();
     });
-	
 	}
 	
 	m = (new Date()).getMinutes();
 	h = (new Date()).getHours();
 	
-	if ((h = 15) && (m <= 12)) { // 23:00 - 24:00
+	if ((h = 15) && (m <= 11)) { // 23:00 - 24:00
 // 日の出・日没時間の洗い替え。
     address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everySun_TARGET003.htm"; //これはcookなので、変えること。
     page.open(address, function (status) {
@@ -53,14 +53,14 @@ var page = require('webpage').create(),
             }));
             console.log('Loading time ' + m);
         }
+	phantom.exit();
     });
-	
 	}
 	
 	m = (new Date()).getMinutes();	
 	h = (new Date()).getHours();
 	
-	if ((h = 16) && (m <= 12)) { // 24:00 - 25:00
+	if ((h = 16) && (m <= 11)) { // 24:00 - 25:00
 // Scoreを「かきまぜる」役目。	
     address = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everyBatch_Score_TARGET.htm"; //これはcookなので、変えること。
     page.open(address, function (status) {
@@ -73,8 +73,8 @@ var page = require('webpage').create(),
             }));
             console.log('Loading time ' + m);
         }
+	phantom.exit();
     });
-	
 	}
 
 	m = (new Date()).getMinutes();		
@@ -93,5 +93,5 @@ var page = require('webpage').create(),
             console.log('TIME: M:' + m);
         }
     });
-//  phantom.exit();	
+    phantom.exit();	
 	}
