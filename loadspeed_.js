6ce5e7914d6412505267e1120a8687f6ebfed23a
pitagoras <root@joypod.net>
2013-09-16 00:42:15 +0900

    iii

--- loadspeed_.js
+++ loadspeed_.js
@@ -3,6 +3,19 @@ var page = require('webpage').create(),
     t, address;
 
     t = Date.now();
+	
+    address1 = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everyBatch_TARGET003.htm"; //これはcookなので、変えること。
+    page.open(address1, function (status) {
+        if (status !== 'success') {
+            console.log('FAIL to load the address');
+        } else {
+            t = Date.now() - t;
+            console.log('Page title is ' + page.evaluate(function () {
+                return document.title;
+            }));
+            console.log('Loading time ' + t + ' msec');
+        }
+    });		
 
 	m = (new Date()).getMinutes();	
 	h = (new Date()).getHours();
