5c4189437b738227092f0cf6a492f4ff84b6017e
pitagoras <root@joypod.net>
2013-09-22 00:11:59 +0900

    timing

--- loadspeed_.js
+++ loadspeed_.js
@@ -20,8 +20,8 @@ var page = require('webpage').create(),
 	m = (new Date()).getMinutes();	
 	h = (new Date()).getHours();
 
-	if (h == 12) {
-		if (m <= 10) { // 21:10 -
+	if (h == 11) {
+		if (m <= 10) { // 20:10 -
     address2 = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-Daily_TARGET.htm"; //これはcookなので、変えること。
     page.open(address2, function (status) {
         if (status !== 'success') {
@@ -43,8 +43,8 @@ var page = require('webpage').create(),
 	h = (new Date()).getHours();
 	
 	
-	if (h == 13) {
-		if (m <= 10) { // 22:10 -
+	if (h == 12) {
+		if (m <= 10) { // 21:10 -
 // 日の出・日没時間の洗い替え。
     address3 = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everySun_TARGET003.htm"; //これはcookなので、変えること。
     page.open(address3, function (status) {
@@ -66,8 +66,8 @@ var page = require('webpage').create(),
 	m = (new Date()).getMinutes();	
 	h = (new Date()).getHours();
 	
-	if (h == 14) {
-		if (m <= 10) { // 23:10 -
+	if (h == 13) {
+		if (m <= 10) { // 22:10 -
 // Scoreを「かきまぜる」役目。	
     address4 = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-everyBatch_Score_TARGET.htm"; //これはcookなので、変えること。
     page.open(address4, function (status) {
@@ -88,7 +88,8 @@ var page = require('webpage').create(),
 
 	m = (new Date()).getMinutes();		
 
-	if (m >= 50) {
+	if (m >= 30) {
+		if (m <= 39){
 	
     address5 = "http://16.sync-playz.appspot.com/user1-aghbc/3.3-Weather_TARGET.htm"; //これはcookなので、変えること。
     page.open(address5, function (status) {
@@ -103,5 +104,6 @@ var page = require('webpage').create(),
         }
     });
 //  phantom.exit();
-	
+
+		}
 	}
